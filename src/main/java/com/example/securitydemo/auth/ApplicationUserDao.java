29fa92f657c5c5ae700c28675d3391fdd6e4bfc5
partellantas@hotmail.com <partellantas@hotmail.com>
2023-01-31 15:24:08 +0100

    second

---
+++ src/main/java/com/example/securitydemo/auth/ApplicationUserDao.java
@@ -0,0 +1,7 @@
+package com.example.securitydemo.auth;
+
+import java.util.Optional;
+
+public interface ApplicationUserDao {
+    Optional<ApplicationUser> selectApplicationUserByUsername(String username);
+}
